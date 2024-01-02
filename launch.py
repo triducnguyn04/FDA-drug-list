a3ef0b59c01137803683095de2398a2c15515516
Tri Duc Nguyen <137370596+triducnguyn04@users.noreply.github.com>
2024-01-01 20:27:44 -0500

    Update launch.py

--- launch.py
+++ launch.py
@@ -3,3 +3,12 @@ Basic functions of launch.py
 1) Initialize graphics and settings
 2) Update database
 '''
+import cache
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/database')
+from database import database_update
+
+# database_update
+if cache.Settings.update_database == True:
+    database_update.update()
